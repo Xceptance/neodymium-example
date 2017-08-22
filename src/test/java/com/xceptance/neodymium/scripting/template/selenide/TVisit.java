1c26fb91025e92ea39c48e710ca3b29095b81bde
Marcus Kaufmann <m.kaufmann@xceptance.net>
2017-08-22 19:22:15 +0200

    added neodymium-library dependency for multi-browser support

--- src/test/java/com/xceptance/neodymium/scripting/template/selenide/TVisit.java
+++ src/test/java/com/xceptance/neodymium/scripting/template/selenide/TVisit.java
@@ -1,11 +1,19 @@
 package com.xceptance.neodymium.scripting.template.selenide;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.xceptance.multibrowser.annotation.TestTargets;
+import com.xceptance.multibrowser.runner.AnnotationRunner;
 import com.xceptance.neodymium.scripting.template.selenide.flow.FOpenHomepage;
 import com.xceptance.neodymium.scripting.template.selenide.page.HomePage;
 
-public class TVisit extends BasicTest
+@TestTargets(
+{
+  "Chrome_1024x768", "FF_1024x768"
+})
+@RunWith(AnnotationRunner.class)
+public class TVisit
 {
 
     @Test
