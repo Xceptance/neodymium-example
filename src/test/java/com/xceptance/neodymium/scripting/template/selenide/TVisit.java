cccb1b82d65771b7d8d75df56780c69a809df096
Marcus Kaufmann <m.kaufmann@xceptance.net>
2017-08-23 11:42:06 +0200

    moved @RunWith to BasicTest class and removed unreachable code

--- src/test/java/com/xceptance/neodymium/scripting/template/selenide/TVisit.java
+++ src/test/java/com/xceptance/neodymium/scripting/template/selenide/TVisit.java
@@ -1,9 +1,7 @@
 package com.xceptance.neodymium.scripting.template.selenide;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import com.xceptance.multibrowser.AnnotationRunner;
 import com.xceptance.multibrowser.TestTargets;
 import com.xceptance.neodymium.scripting.template.selenide.flow.FOpenHomepage;
 import com.xceptance.neodymium.scripting.template.selenide.page.HomePage;
@@ -12,8 +10,7 @@ import com.xceptance.neodymium.scripting.template.selenide.page.HomePage;
 {
   "Chrome_1024x768", "FF_1024x768"
 })
-@RunWith(AnnotationRunner.class)
-public class TVisit
+public class TVisit extends BasicTest
 {
 
     @Test
