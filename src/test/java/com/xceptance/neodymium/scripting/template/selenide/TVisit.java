6b9ecf1e5d132cee2671571eb0e1eb0d4a11f2eb
Marcel Pfotenhauer <m.pfotenhauer@xceptance.net>
2017-09-01 10:15:17 +0200

    Removed parameters(data sets) since the libary is not ready yet

--- src/test/java/com/xceptance/neodymium/scripting/template/selenide/TVisit.java
+++ src/test/java/com/xceptance/neodymium/scripting/template/selenide/TVisit.java
@@ -1,12 +1,6 @@
 package com.xceptance.neodymium.scripting.template.selenide;
 
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
-
 import org.junit.Test;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
 
 import com.xceptance.multibrowser.TestTargets;
 import com.xceptance.neodymium.scripting.template.selenide.flow.FOpenHomepage;
@@ -18,43 +12,11 @@ import com.xceptance.neodymium.scripting.template.selenide.page.PHome;
 })
 public class TVisit extends BasicTest
 {
-    @Parameter
-    public String data;
-
-    @Parameters(name = "{index}::{0}")
-    public static Collection<Object[]> getTestData()
-    {
-        List<Object[]> data = new LinkedList<>();
-
-        data.add(new Object[]
-        {
-          "a"
-        });
-        data.add(new Object[]
-        {
-          "b"
-        });
-
-        return data;
-    }
-
     @Test
     public void test()
     {
         PHome homePage = new FOpenHomepage().flow();
         homePage.validate();
         homePage.footer().validate();
-    }
-
-    @Test
-    public void test1()
-    {
-
-    }
-
-    @Test
-    public void test2()
-    {
-
     }
 }
