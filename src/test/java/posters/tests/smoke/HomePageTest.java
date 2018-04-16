dbaf7e4ae8d97adbd97c78d4e6de0d57fd003d6e
Marcel Pfotenhauer <m.pfotenhauer@xceptance.net>
2018-04-16 10:25:20 +0200

    showcased allure: link, issue and tms tags

--- src/test/java/posters/tests/smoke/HomePageTest.java
+++ src/test/java/posters/tests/smoke/HomePageTest.java
@@ -2,9 +2,12 @@ package posters.tests.smoke;
 
 import org.junit.Test;
 
+import io.qameta.allure.Issue;
+import io.qameta.allure.Link;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.TmsLink;
 import io.qameta.allure.junit4.Tag;
 import posters.flows.OpenHomePageFlow;
 import posters.pageobjects.pages.browsing.HomePage;
@@ -16,6 +19,9 @@ import posters.tests.AbstractTest;
 @Severity(SeverityLevel.TRIVIAL)
 @Owner("Joe Fix")
 @Tag("smoke")
+@TmsLink("end-of-script-developer-and-now")
+@Issue("148")
+@Link(url = "https://ask.xceptance.de/t/end-of-script-developer-and-now/148", type = "custom", name = "DemoLink")
 public class HomePageTest extends AbstractTest
 {
     @Test
