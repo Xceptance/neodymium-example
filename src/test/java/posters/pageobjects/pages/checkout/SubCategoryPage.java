44f9ec88328dfe52378066b9644e94115bbfd4c4
ischonger <i.schaefer@xceptance.net>
2018-04-30 11:31:28 +0200

    refactored BrowseRandom

---
+++ src/test/java/posters/pageobjects/pages/checkout/SubCategoryPage.java
@@ -0,0 +1,97 @@
+package posters.pageobjects.pages.checkout;
+
+import static com.codeborne.selenide.Selenide.$;
+
+import java.util.Random;
+
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.SelenideElement;
+
+/**
+ * @author schaefer
+ */
+public class SubCategoryPage extends AbstractCheckoutPage
+{
+    private static Random random;
+
+    private int categoryPositionX;
+
+    private int categoryPositionY;
+
+    private int productPositionX;
+
+    private int productPositionY;
+
+    private static SelenideElement pagination = $("#pagination-bottom");
+
+    public SubCategoryPage(long seed)
+    {
+        random = new Random(seed);
+    }
+
+    public int getCategoryPositionX()
+    {
+        return categoryPositionX;
+    }
+
+    public int getCategoryPositionY()
+    {
+        return categoryPositionY;
+    }
+
+    public int getProductPositionX()
+    {
+        return productPositionX;
+    }
+
+    public int getProductPositionY()
+    {
+        return productPositionY;
+    }
+
+    public void choseCategory()
+    {
+        // compute random horizontal category position (for posters: random value from 1 to 4)
+        // 1: World of nature
+        // 2: Dining
+        // 3: Transportation
+        // 4: Panoramas
+        categoryPositionX = random.nextInt(3) + 1;
+
+        // compute random vertical category position (for posters: random value from 1 to 4)
+        // if (categoryPositionX == 4) == true then categoryPositionY has range from 1 to 4
+        // otherwise from 1 to 3, because any category has 3 subcategories instead of category 4,
+        // which has 4
+        if (categoryPositionX == 4)
+        {
+            categoryPositionY = random.nextInt(3) + 1;
+        }
+        else
+        {
+            categoryPositionY = random.nextInt(2) + 1;
+        }
+
+    }
+
+    public void goToOverviewPage()
+    {
+        String stringOfPaginationChildren = pagination.getText();
+        stringOfPaginationChildren = stringOfPaginationChildren.replace(">", "");
+        stringOfPaginationChildren = stringOfPaginationChildren.replace("\n", "");
+
+        int amountOfProductOverviewPages = stringOfPaginationChildren.length();
+        int intChosenElementValue = random.nextInt(amountOfProductOverviewPages) + 1;
+
+        String stringChosenElementValue = String.valueOf(intChosenElementValue);
+
+        SelenideElement chosenSelenideElement = pagination.$(Selectors.withText(stringChosenElementValue));
+        chosenSelenideElement.scrollTo();
+        chosenSelenideElement.click();
+    }
+
+    public void choseDetailPage()
+    {
+        productPositionX = random.nextInt(2) + 1;
+        productPositionY = random.nextInt(1) + 1;
+    }
+}
--- src/test/java/posters/tests/smoke/BrowseRandom.java
+++ src/test/java/posters/tests/smoke/BrowseRandom.java
@@ -3,15 +3,9 @@
  */
 package posters.tests.smoke;
 
-import static com.codeborne.selenide.Selenide.$;
-
-import java.util.Random;
-
 import org.junit.Before;
 import org.junit.Test;
 
-import com.codeborne.selenide.Selectors;
-import com.codeborne.selenide.SelenideElement;
 import com.xceptance.neodymium.util.DataUtils;
 
 import io.qameta.allure.Owner;
@@ -22,6 +16,7 @@ import posters.flows.OpenHomePageFlow;
 import posters.pageobjects.pages.browsing.CategoryPage;
 import posters.pageobjects.pages.browsing.HomePage;
 import posters.pageobjects.pages.browsing.ProductdetailPage;
+import posters.pageobjects.pages.checkout.SubCategoryPage;
 import posters.tests.AbstractTest;
 
 /**
@@ -34,71 +29,41 @@ public class BrowseRandom extends AbstractTest
 {
     private static int numberOfProductDetailPages;
 
-    private static long seed;
-
-    private static Random random;
+    private SubCategoryPage subCategoryPage;
 
     @Before
     public void setup()
     {
         numberOfProductDetailPages = DataUtils.asInt("numberOfProductDetailPages");
-        seed = DataUtils.asLong("seed");
+        subCategoryPage = new SubCategoryPage(DataUtils.asLong("seed"));
     }
 
     @Test
     public void searchProductDetailPage()
     {
-        random = new Random(seed);
-
         for (int i = 1; i <= numberOfProductDetailPages; i++)
         {
-            // compute random horizontal category position (for posters: random value from 1 to 4)
-            // 1: World of nature
-            // 2: Dining
-            // 3: Transportation
-            // 4: Panoramas
-            int categoryPositionX = random.nextInt(3) + 1;
-
-            // compute random vertical category position (for posters: random value from 1 to 4)
-            // if (categoryPositionX == 4) == true then categoryPositionY has range from 1 to 4
-            // otherwise from 1 to 3, because any category has 3 subcategories instead of category 4,
-            // which has 4
-            int categoryPositionY;
-            if (categoryPositionX == 4)
-            {
-                categoryPositionY = random.nextInt(3) + 1;
-            }
-            else
-            {
-                categoryPositionY = random.nextInt(2) + 1;
-            }
-
             // Goto homepage
             HomePage homePage = OpenHomePageFlow.flow();
             homePage.validate();
 
             // Goto subcategory
-            final String categoryName = homePage.topNav.getSubCategoryNameByPosition(categoryPositionX, categoryPositionY);
-            CategoryPage categoryPage = homePage.topNav.clickSubCategoryByPosition(categoryPositionX, categoryPositionY);
+            subCategoryPage.choseCategory();
+            final String categoryName = homePage.topNav.getSubCategoryNameByPosition(subCategoryPage.getCategoryPositionX(),
+                                                                                     subCategoryPage.getCategoryPositionY());
+            CategoryPage categoryPage = homePage.topNav.clickSubCategoryByPosition(subCategoryPage.getCategoryPositionX(),
+                                                                                   subCategoryPage.getCategoryPositionY());
             categoryPage.validate(categoryName);
 
             // Goto any product overview page
-            SelenideElement pagination = $("#pagination-bottom");
-            String stringOfPaginationChildren = pagination.getText();
-            stringOfPaginationChildren = stringOfPaginationChildren.replace(">", "");
-            stringOfPaginationChildren = stringOfPaginationChildren.replace("\n", "");
-            int amountOfProductOverviewPages = stringOfPaginationChildren.length();
-            int intChosenElementValue = random.nextInt(amountOfProductOverviewPages) + 1;
-            String stringChosenElementValue = String.valueOf(intChosenElementValue);
-            SelenideElement chosenSelenideElement = pagination.$(Selectors.withText(stringChosenElementValue));
-            chosenSelenideElement.scrollTo();
-            chosenSelenideElement.click();
+            subCategoryPage.goToOverviewPage();
 
             // Goto product detail page
-            int productPositionX = random.nextInt(2) + 1;
-            int productPositionY = random.nextInt(1) + 1;
-            final String productName = categoryPage.getProductNameByPosition(productPositionX, productPositionY);
-            ProductdetailPage productPage = categoryPage.clickProductByPosition(productPositionX, productPositionY);
+            subCategoryPage.choseDetailPage();
+            final String productName = categoryPage.getProductNameByPosition(subCategoryPage.getProductPositionX(),
+                                                                             subCategoryPage.getProductPositionY());
+            ProductdetailPage productPage = categoryPage.clickProductByPosition(subCategoryPage.getProductPositionX(),
+                                                                                subCategoryPage.getProductPositionY());
             productPage.validate(productName);
         }
 
