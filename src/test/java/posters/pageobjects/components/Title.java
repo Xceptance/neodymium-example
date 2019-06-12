2ac35e31e5804bd8c860757f8d7ca77c2a7e5340
Marcel Pfotenhauer <m.pfotenhauer@xceptance.net>
2019-06-12 14:52:40 +0200

    added logging showcase

--- src/test/java/posters/pageobjects/components/Title.java
+++ src/test/java/posters/pageobjects/components/Title.java
@@ -3,6 +3,8 @@ package posters.pageobjects.components;
 import static com.codeborne.selenide.Selenide.title;
 
 import org.junit.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.xceptance.neodymium.util.SelenideAddons;
 
@@ -21,5 +23,12 @@ public class Title extends AbstractComponent
         SelenideAddons.wrapAssertionError(() -> {
             Assert.assertEquals(title, title());
         });
+
+        Logger logger = LoggerFactory.getLogger(getClass());
+        logger.trace("trace");
+        logger.debug("debug");
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
     }
 }
