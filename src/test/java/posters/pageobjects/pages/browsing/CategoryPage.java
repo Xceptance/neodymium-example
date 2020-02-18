6aaf1277815ee07e50fad8fec1e8990def9ecb7a
olha <olha@olha>
2020-02-18 11:23:23 +0100

    deleted unused methods in CategoryPage

--- src/test/java/posters/pageobjects/pages/browsing/CategoryPage.java
+++ src/test/java/posters/pageobjects/pages/browsing/CategoryPage.java
@@ -12,9 +12,6 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
-import java.util.Random;
-
-import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import com.xceptance.neodymium.util.Neodymium;
 
@@ -62,52 +59,6 @@ public class CategoryPage extends AbstractBrowsingPage
     }
 
     /**
-     * @param row
-     * @param column
-     * @return
-     */
-    @Step("click on a product by position in grid")
-    public ProductdetailPage clickProductByPosition(int row, int column)
-    {
-        // Open the product detail page
-        // Clicks a product by position. Because of the html code, this requires x and y coordinates.
-        SelenideElement rowContainer = $$("#productOverview > .row").get(row - 1);
-        rowContainer.find(".thumbnail", column - 1).scrollTo().click();
-        ProductdetailPage pdp = new ProductdetailPage();
-        pdp.isExpectedPage();
-        return pdp;
-    }
-
-    /**
-     * @param row
-     * @param column
-     * @return
-     */
-    @Step("get a product name by position in grid")
-    public String getProductNameByPosition(int row, int column)
-    {
-        SelenideElement rowContainer = productOverview.findAll(".thumbnails.row").get(row - 1);
-        return rowContainer.find("h4.pName", column - 1).text();
-    }
-
-    /**
-     * @param position
-     * @return
-     */
-    @Step("click on a product by position")
-    public ProductdetailPage clickProductByPosition(int position)
-    {
-        final int index = position - 1;
-        // Open the product detail page
-        // Click on the product's image and open the product overview page
-        // Click the product link to open the product detail page
-        $("#product" + index + " img").scrollTo().click();
-        ProductdetailPage pdp = new ProductdetailPage();
-        pdp.isExpectedPage();
-        return pdp;
-    }
-
-    /**
      * @param position
      * @return
      */
@@ -121,18 +72,6 @@ public class CategoryPage extends AbstractBrowsingPage
         ProductdetailPage pdp = new ProductdetailPage();
         pdp.isExpectedPage();
         return pdp;
-    }
-
-    /**
-     * @param position
-     * @return
-     */
-    @Step("get a product name by position")
-    public String getProductNameByPosition(int position)
-    {
-        final int index = position - 1;
-        // Get the product name
-        return $("#product" + index + " .pInfo .pName").text();
     }
 
     /**
@@ -163,34 +102,6 @@ public class CategoryPage extends AbstractBrowsingPage
     }
 
     /**
-     * @param searchTerm
-     * @param searchTermExpectedCount
-     */
-    @Step("validate search results for \"{searchTerm}\" on category page")
-    public CategoryPage validateSearchHits(String searchTerm, int searchTermExpectedCount)
-    {
-        $("#titleSearchText").should(exist);
-        // Validate the headline contains the search phrase
-        // \\(" + searchTermExpectedCount + " *\\)
-        $("#titleSearchText").should(matchText(Neodymium.localizedText("search.results.text") + ": '" + searchTerm + "' \\(" + searchTermExpectedCount
-                                               + ".*\\)"));
-        // Verify that the correct search term is displayed
-        // Validate the entered search phrase is still visible in the input
-        search.validateSearchTerm(searchTerm);
-        // Assert the Headline displays the search term.
-        $(".header-container #searchTextValue").shouldHave(exactText(searchTerm));
-        // Verify there are search results
-        // There is at least one row of results
-        $("#productOverview ul.row").shouldBe(exist);
-        // There is at least one product in the results
-        $$("#productOverview li").shouldHave(sizeGreaterThan(0));
-        // Verify that there is the specified amount of results
-        // The amount of products shown in the headline matches the expected value
-        $("#totalProductCount").shouldHave(exactText(Integer.toString(searchTermExpectedCount)));
-        return this;
-    }
-
-    /**
      * @param productName
      */
     @Step("validate product \"{productName}\" is visible on category page")
@@ -203,35 +114,11 @@ public class CategoryPage extends AbstractBrowsingPage
     /**
      * @param categoryName
      */
-    @Step("validate category page of category \"{categoryName}\"")
-    public CategoryPage validate(String categoryName)
-    {
-        validateStructure();
-        validateCategoryName(categoryName);
-        return this;
-    }
-
-    /**
-     * @param categoryName
-     */
     @Step("validate category page of category \"{categoryName}\" and assert visually")
     public CategoryPage validateAndVisualAssert(String categoryName)
     {
         validateStructureAndVisual();
         validateCategoryName(categoryName);
         return this;
-    }
-
-    public String getRandomProductDetailName(Random random)
-    {
-        ElementsCollection rows = productOverview.findAll(".thumbnails.row");
-        int numberOfRows = rows.size();
-        int productIndexX = random.nextInt(numberOfRows);
-
-        SelenideElement row = rows.get(productIndexX);
-        int numberOfColumns = row.findAll(".thumbnail").filter(visible).size();
-        int productIndexY = random.nextInt(numberOfColumns);
-
-        return getProductNameByPosition(productIndexX + 1, productIndexY + 1);
     }
 }
