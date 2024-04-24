505a31e6eb180ec8f1654d31248d1d280b14ad2f
Jonas Hoyer <j.hoyer@xceptance.com>
2024-04-24 17:11:52 +0200

    deleted unused variable

--- src/test/java/posters/flows/CartCleanUpFlow.java
+++ src/test/java/posters/flows/CartCleanUpFlow.java
@@ -21,7 +21,6 @@ public class CartCleanUpFlow
         {
             // go to cart page
             var cartPage = homePage.header.miniCart.openCartPage();
-            int currentCount = cartPage.header.miniCart.getTotalCount();
             
             // remove the first product as long as one is available
             while (cartPage.header.miniCart.getTotalCount() != 0)
